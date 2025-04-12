7e300c3add2af1e860c87fc214bdb64c907098f9
mohitshadija05 <shadijamohit53@gmail.com>
2025-04-12 21:29:44 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/academic-react-hub-main/sw.js', { scope: '/academic-react-hub-main/' })})}
